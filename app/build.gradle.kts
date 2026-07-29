d4a57971a79c288934b03b6e6c134bf1cd7c0872
Osmano Torres <osmano.torres@rensoftware.com.br>
2026-07-29 18:07:10 -0300

    fix: remove signingConfigs (debug ja tem keystore padrao)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,15 +31,6 @@ android {
         }
     }
 
-    signingConfigs {
-        debug {
-            storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
-            storePassword = "android"
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-        }
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
